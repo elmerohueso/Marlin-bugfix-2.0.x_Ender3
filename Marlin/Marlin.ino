94d75098b6118e4919c04f122c96287b8c6f0fd1
elmerohueso <elmerohueso@users.noreply.github.com>
2019-12-15 16:07:31 -0700

    Fix revert

--- Marlin/Marlin.ino
+++ Marlin/Marlin.ino
@@ -7,16 +7,11 @@
  * 11222019_1 - Center the leveling grid on the bed
  * 11222019_2 - Updated source for leveling math bug
  * 11222019_3 - New status screen logo
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
  * 11232019_1 - Revert status screen logo as it doesn't look great
  * 11232019_2 - Increase probe grid to 4x4
  * 11272019_1 - Increase probe grid to 5x5
  * 11272019_2 - Add hotend values from PID autotune
->>>>>>> parent of 4d1d166...  Marlin 2.0 release
-=======
->>>>>>> parent of 97c2672... Final!????
+
 ================================================================================
 
   Marlin Firmware

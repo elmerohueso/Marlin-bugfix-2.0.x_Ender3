754ad08b89063e0b4c674ea5f7a49bd64aaceb65
elmerohueso <elmerohueso@users.noreply.github.com>
2019-12-15 16:03:12 -0700

    Updated to Marlin 2.0 release

--- Marlin/Marlin.ino
+++ Marlin/Marlin.ino
@@ -10,11 +10,14 @@
  * 11222019_2 - Updated source for leveling math bug
  * 11222019_3 - New status screen logo
 <<<<<<< HEAD
+<<<<<<< HEAD
  * 11232019_1 - Revert status screen logo as it doesn't look great
  * 11232019_2 - Increase probe grid to 4x4
  * 11272019_1 - Increase probe grid to 5x5
  * 11272019_2 - Add hotend values from PID autotune
  * 12152019_1 - Updated to Marlin 2.0 release
+=======
+>>>>>>> parent of 97c2672... Final!????
 =======
 >>>>>>> parent of 97c2672... Final!????
 ================================================================================
